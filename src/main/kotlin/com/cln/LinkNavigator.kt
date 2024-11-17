e70de65b82bf7a5b8f0305e5f045b09dcd8ece63
natrix <natrix>
2024-11-17 22:29:16 +0300

    added new regex. New navigator alg

--- src/main/kotlin/com/cln/LinkNavigator.kt
+++ src/main/kotlin/com/cln/LinkNavigator.kt
@@ -15,7 +15,7 @@ import java.awt.event.MouseEvent
 class LinkNavigator : EditorMouseListener {
     override fun mouseClicked(e: EditorMouseEvent) {
         println("call LinkNavigator mouseClicked")
-        if (e.mouseEvent.isAltDown && e.mouseEvent.clickCount > 1 && e.mouseEvent.button ==  MouseEvent.BUTTON1) {
+        if (e.mouseEvent.isAltDown && e.mouseEvent.clickCount > 1 && e.mouseEvent.button == MouseEvent.BUTTON1) {
             println("ALT + 2 Click detected")
             val editor = e.editor
             val project = editor.project ?: return
@@ -26,23 +26,32 @@ class LinkNavigator : EditorMouseListener {
             val lineEndOffset = document.getLineEndOffset(lineNumber)
             val lineText = document.getText(TextRange(lineStartOffset, lineEndOffset))
 
-            val regex = Regex("#\\s*/(.+):(\\d+)")
+            val regex = Regex("#\\s*/(.+):(\\d+)|\\(file:///(.+):(\\d+)\\)")
             val matchResult = regex.find(lineText) ?: return
-            val (filePath, line) = matchResult.destructured
-            if (line.toInt() <= 0) {
+            val (filePath1, line1, filePath2, line2) = matchResult.destructured
+
+            val filePath = filePath1.ifEmpty { filePath2 }
+            val line = line1.ifEmpty { line2 }
+
+            if (filePath.isEmpty() || line.isEmpty() || line.toIntOrNull() == null || line.toInt() <= 0) {
                 return
             }
+
             navigateToFile(project, filePath, line.toInt())
         }
     }
 
     private fun navigateToFile(project: Project, filePath: String, line: Int) {
         println("call LinkNavigator navigateToFile")
-        val projectBasePath = project.basePath ?: return
-        val absoluteFilePath = "$projectBasePath/$filePath"
-        val file = LocalFileSystem.getInstance().findFileByPath(absoluteFilePath) ?: return
-        val psiFile = PsiManager.getInstance(project).findFile(file) ?: return
-        openFileAtLine(project, psiFile, line)
+        val file = LocalFileSystem.getInstance().findFileByPath(filePath)
+        if (file == null) {
+            val projectBasePath = project.basePath ?: return
+            val absoluteFilePath = "$projectBasePath/$filePath"
+            val fileFromProjectRoot = LocalFileSystem.getInstance().findFileByPath(absoluteFilePath) ?: return
+            openFileAtLine(project, PsiManager.getInstance(project).findFile(fileFromProjectRoot) ?: return, line)
+        } else {
+            openFileAtLine(project, PsiManager.getInstance(project).findFile(file) ?: return, line)
+        }
     }
 
     private fun openFileAtLine(project: Project, psiFile: PsiFile, line: Int) {
